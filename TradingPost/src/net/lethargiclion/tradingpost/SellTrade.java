bc8eb2f92cb90a97ac9f0db55f3756b0e7316a95
TerrorBite <terrorbite@lethargiclion.net>
2012-02-05 17:07:57 +1000

    Started work on SellTrade class

--- TradingPost/src/net/lethargiclion/tradingpost/SellTrade.java
+++ TradingPost/src/net/lethargiclion/tradingpost/SellTrade.java
@@ -1,8 +1,11 @@
 package net.lethargiclion.tradingpost;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -10,12 +13,22 @@ import org.bukkit.inventory.ItemStack;
  * @author TerrorBite
  *
  */
-public class SellTrade {
+public class SellTrade implements ConfigurationSerializable {
 	
-	ArrayList<ItemBid> bids;
+	List<ItemBid> bids = new ArrayList<ItemBid>();
+	List<ItemStack> items = new ArrayList<ItemStack>();
+	OfflinePlayer owner;
+	TradeStatus status;
+	int AcceptedBidID;
 	
 	public SellTrade(OfflinePlayer p, ItemStack[] items) {
 		
+	}
+
+	@Override
+	public Map<String, Object> serialize() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
---
+++ TradingPost/src/net/lethargiclion/tradingpost/TradeStatus.java
@@ -0,0 +1,8 @@
+package net.lethargiclion.tradingpost;
+
+public enum TradeStatus {
+	open,
+	completed,
+	withdrawn,
+	expired
+}
